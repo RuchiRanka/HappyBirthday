763a0974ab3efe38253c7d15ec0c40d5c94d17ad
RuchiRanka <ruchiranka2103@gmail.com>
2024-11-16 16:02:03 +0530

    commit3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Games from "./components/Games";
 import Gifts from "./components/Gifts";
 import MemoryLane from "./components/MemoryLane";
@@ -11,20 +11,38 @@ function App() {
   if (sessionStorage.getItem("gamesSolved") === null) {
     sessionStorage.setItem("gamesSolved", "false,false,false,false,false");
   }
+  const shouldRedirect = true;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (shouldRedirect) {
+      navigate("/home");
+    }
+  });
   return (
     <>
       <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
       <div className="App">
         <Navbar></Navbar>
-        <Routes>
-          <Route
-            path={process.env.REACT_APP_FOR_PATH + "/home"}
-            element={<Home />}
-          ></Route>
-          <Route path="/memory-lane" element={<MemoryLane />}></Route>
-          <Route path="/games" element={<Games />}></Route>
-          <Route path="/gifts" element={<Gifts />}></Route>
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path={process.env.REACT_APP_FOR_PATH + "/home"}
+              element={<Home />}
+            ></Route>
+            <Route
+              path={process.env.REACT_APP_FOR_PATH + "/memory-lane"}
+              element={<MemoryLane />}
+            ></Route>
+            <Route
+              path={process.env.REACT_APP_FOR_PATH + "/games"}
+              element={<Games />}
+            ></Route>
+            <Route
+              path={process.env.REACT_APP_FOR_PATH + "/gifts"}
+              element={<Gifts />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
