365f8755882aa697438ae6bbd815a1bf84fee8c9
RuchiRanka <ruchiranka2103@gmail.com>
2024-11-19 22:49:59 +0530

    commit9

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,16 @@ function App() {
       <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
       <div className="App">
         <Navbar></Navbar>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/memory-lane" element={<MemoryLane />}></Route>
-            <Route path="/games" element={<Games />}></Route>
-            <Route path="/gifts" element={<Gifts />}></Route>
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </BrowserRouter>
+        {/* <BrowserRouter> */}
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/memory-lane" element={<MemoryLane />}></Route>
+          <Route path="/games" element={<Games />}></Route>
+          <Route path="/gifts" element={<Gifts />}></Route>
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+        {/* </BrowserRouter> */}
       </div>
     </>
   );
