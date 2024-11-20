25cd5c8ff5ec2d67dfeb23856f3c958e9a5cd9a6
RuchiRanka <ruchiranka2103@gmail.com>
2024-11-20 12:16:26 +0530

    BrowserRouter issue fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -20,17 +20,17 @@ function App() {
     <>
       <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
       <div className="App">
-        <Router basename="/HappyBirthday">
-          <Navbar></Navbar>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/memory-lane" element={<MemoryLane />}></Route>
-            <Route path="/games" element={<Games />}></Route>
-            <Route path="/gifts" element={<Gifts />}></Route>
-            <Route path="*" element={<Navigate to="/home" />} />
-          </Routes>
-        </Router>
+        {/* <Router basename="/HappyBirthday"> */}
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/memory-lane" element={<MemoryLane />}></Route>
+          <Route path="/games" element={<Games />}></Route>
+          <Route path="/gifts" element={<Gifts />}></Route>
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+        {/* </Router> */}
       </div>
     </>
   );
