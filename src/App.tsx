71d215d4b268ca187c979c28332e8c73b4cde6d8
RuchiRanka <ruchiranka2103@gmail.com>
2024-11-20 13:16:21 +0530

    BrowserRouter issue fix 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import {
-  HashRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Games from "./components/Games";
 import Gifts from "./components/Gifts";
 import MemoryLane from "./components/MemoryLane";
@@ -20,17 +15,15 @@ function App() {
     <>
       <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
       <div className="App">
-        <Router basename="/HappyBirthday">
-          <Navbar></Navbar>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/memory-lane" element={<MemoryLane />}></Route>
-            <Route path="/games" element={<Games />}></Route>
-            <Route path="/gifts" element={<Gifts />}></Route>
-            <Route path="*" element={<Navigate to="/home" />} />
-          </Routes>
-        </Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/memory-lane" element={<MemoryLane />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/gifts" element={<Gifts />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
       </div>
     </>
   );
